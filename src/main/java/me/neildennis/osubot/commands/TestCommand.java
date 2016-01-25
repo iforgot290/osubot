5dd0ea647672c78f2c0048d61e70e7836a06caab
Neil <iforgot290@gmail.com>
2016-01-25 09:42:15 -0600

    updated

--- src/main/java/me/neildennis/osubot/commands/TestCommand.java
+++ src/main/java/me/neildennis/osubot/commands/TestCommand.java
@@ -6,14 +6,7 @@ public class TestCommand extends Command {
 
 	@Override
 	public void handle(String[] args, User user) {
-		StringBuilder build = new StringBuilder();
-		for (int i = 1; i < args.length; i++){
-			build.append(args[i]);
-			if (i!=args.length)
-				build.append(" ");
-		}
 		
-		user.send().message(build.toString());
 	}
 
 }
